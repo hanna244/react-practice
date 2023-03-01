42de6e0875aeaa2a83cae3dd9f2031d875cdba1d
hanna244 <shhn0509@gmail.com>
2023-03-01 23:59:30 +0900

    setting: CRA 세팅

---
+++ real-world/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis atque
+      perferendis, totam ea magnam perspiciatis, repellat magni consequuntur
+      molestias mollitia eius amet animi? Quisquam harum eius nisi iste officiis
+      tenetur.
+    </div>
+  );
+};
+
+export default Login;
